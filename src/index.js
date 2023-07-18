d406d5e8e975d95edb3bea28ffcc38a4d360e671
Juan B <jjuanbr97@gmail.com>
2023-07-18 01:00:23 -0300

    Se anexo um comentario e novo codigo en js, dando una nova opcion melhor formulada

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 let formulario = document.querySelectorAll(".texbox");
 let validation = document.getElementById("btn-formulario");
 let msjAlert = document.querySelectorAll(".notification");
-
 function validarCampo() {
   formulario.forEach((respuesta, i) => {
     addEventListener("change", function () {
@@ -15,7 +14,6 @@ function validarCampo() {
     });
   });
 }
-
 validation.addEventListener("click", function () {
   for (let i = 0; i < formulario.length; i++) {
     let validationForm = formulario[i].value;
@@ -29,3 +27,35 @@ validation.addEventListener("click", function () {
   }
   validarCampo();
 });
+
+
+
+
+
+// ja de tanto fazer este codigo investigando poco, practicando para entender consegui fazer um poco mais compacto. mais o codigo de encima foi o primero que saio.
+
+/*
+
+function reglaIf(parametro, i) {
+  if (parametro.value.length > 0) {
+    formulario[i].classList.add("valid");
+    msjAlert[i].textContent = "";
+  } else {
+    formulario[i].classList.remove("valid");
+    msjAlert[i].textContent = "campo obrigatório";
+  }
+}
+
+
+
+validation.addEventListener("click", function () {
+  formulario.forEach((parametro2, i) => {
+    reglaIf(parametro2, i);
+    formulario[i].addEventListener("change", function () {
+      reglaIf(parametro2, i);
+    });
+  });
+});
+
+
+*/
